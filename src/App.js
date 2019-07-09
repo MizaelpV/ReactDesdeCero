5bdf1d39e9655267e326eaeaf829a3d43087a7c8
Mizael Paredes <mizael.paredes@externos.bci.cl>
2019-07-09 16:30:07 -0400

    Inclusion de propTypes

--- src/App.js
+++ src/App.js
@@ -19,18 +19,21 @@ const App = () => (
   </div>
 </div>
 <div className="ed-grid m-grid-3">
-  <Curso />
-  <Curso /> 
-  <Curso />
-  <Curso />
-  <Curso />
+  <Curso 
+title="React será mi comienzo en el Desarrollo formal"
+image="https://image.freepik.com/free-vector/colorful-watercolor-background_79603-99.jpg"
+price="50usd"
+nombre="Mizael"
+apellido="Paredes"
+imageTiny="https://image.shutterstock.com/image-vector/art-deco-seamless-vintage-wallpaper-600w-352792943.jpg"
+    />
 </div>
 </>
 )
 
 export default App;
 /*Reglas JSX 
-1: Todas as etiquetas deben cerrarse 
+1: Todas las etiquetas deben cerrarse 
 2: Los componentes deben retornar un solo elemento padre
 3: Apoyarse de los Fragments => <> Hijos </>
 5: Etiqueta img siempre debe cerrarse 
@@ -38,7 +41,8 @@ export default App;
 7: for => htmlFor
 */
 /*Cómo subir proyecto a github
-
-
+1: configurar llave ssh
+2: Crear manualmente el repositorio en el github
+3: Ejecutar a través de consola y hacer push hacia tu repositorio
 
 */ 
--- src/curso.jsx
+++ src/curso.jsx
@@ -1,29 +1,52 @@
 import React from 'react'
+import Proptypes from 'prop-types'
 
-const Curso = () => (
+
+
+
+
+const Curso = ({title, image, price, nombre, apellido, imageTiny}) => (
+    <>  
     <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://img.freepik.com/free-vector/colorful-gradient-geometric-shapes-background_23-2148191034.jpg?size=626&ext=jpg" alt="posteruno"/>
+      <img src={image} alt={title}/>
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
       <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
+          {title}
       </h3>
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg" alt="lion"/>
+              <img src={imageTiny} alt="lion"/>
             </div>
           </div>
-          <span className="small">Alexys Lozada</span>
+          <span className="small">{`${nombre} ${apellido}`}</span>
         </div>
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="https://www.google.com">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="https://www.google.com">{`$${price}`}</a>
       </div>
     </div>
   </article>
+  </>
 )
 
+  Curso.propTypes = {
+  title: Proptypes.string,
+  image: Proptypes.string,
+  price: Proptypes.number,
+  nombre:Proptypes.string,
+  apellido: Proptypes.string,
+  imageTiny: Proptypes.string
+}
+Curso.defaultProps ={
+  title: "No se encontró titulo",
+  image: "http://2.bp.blogspot.com/-ndToICeJr7k/VDbmM4LasgI/AAAAAAAAAQI/P8eupazjpdk/s1600/tulum2.jpg",
+  price: 0,
+  nombre: "Mizael",
+  apellido: "Paredes",
+  imageTiny: "https://images.techhive.com/images/article/2016/01/thinkstockphotos-481493125-100638928-large.jpg"
+}
 export default Curso 
